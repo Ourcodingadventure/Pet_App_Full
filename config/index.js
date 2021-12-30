505ab0b1d633f48ecb97e7abc38179a5c7df3cd6
Ourcodingadventure <Javitan0309@gmail.com>
2021-12-30 19:50:31 +0200

    env

--- config/index.js
+++ config/index.js
@@ -3,4 +3,6 @@ module.exports = {
   PORT: process.env.PORT,
   SERVER_SECRET: process.env.SERVER_SECRET,
   databaseURL: process.env.databaseURL,
+  FBDBURL: process.env.databaseURL,
+  bucketURL: process.env.bucketURL,
 };
--- config/storage.js
+++ config/storage.js
@@ -1,11 +1,11 @@
-const admin = require('firebase-admin')
+const admin = require("firebase-admin");
+const { FBDBURL, bucketURL } = require(".");
 const serviceAccount = require("./firebase.json");
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://webmobile-48ab0.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: FBDBURL,
 });
 
-const bucket = admin.storage().bucket("gs://webmobile-48ab0.appspot.com");
+const bucket = admin.storage().bucket(bucketURL);
 
 module.exports = bucket;
-
